0fe8730d3e602928c45705138a4048bdb76858db
Dermot Mac Conville <c00182936@itcarlow.ie>
2017-12-01 14:37:58 +0000

    added player class, no real implimentation just yet. will start branching soon

--- SSR/SSR/Enemy.h
+++ SSR/SSR/Enemy.h
@@ -1,12 +1,15 @@
 #pragma once
 #include <SFML\Graphics.hpp>
+
 #include "EnemyTypes.h"
 class Enemy
 {
 public:
 	Enemy();
-	~Enemy();
+	virtual ~Enemy() {};
 	int Type;
+	virtual void Draw(sf::RenderWindow &window)=0;
+	virtual void Update() = 0;
 	//might include a pointer to player here, let's each ai requestion position and bool for moving
 };
 
--- SSR/SSR/Player.h
+++ SSR/SSR/Player.h
@@ -1,8 +1,19 @@
 #pragma once
+#include <SFML\Graphics.hpp>
+#include <vector>//might be useful for a vector of bullets
 class Player
 {
 public:
 	Player();
 	~Player();
+	void Draw(sf::RenderWindow &window);
+	void Update();
+	void HandleInput();
+private:
+	sf::Vector2f position;
+	sf::Vector2f velocity;
+	sf::Texture texture;
+	sf::Sprite spr;
+
 };
 
